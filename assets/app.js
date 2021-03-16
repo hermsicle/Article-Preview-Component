3a03c5fc419f5f8305dbcf9d779284374a8cb4f3
Herman Liu <hermanliu95@gmail.com>
2021-03-16 15:58:30 -0700

    fixed bug

--- assets/app.js
+++ assets/app.js
@@ -5,21 +5,24 @@ const socials = document.getElementById('socials')
 const shareBtns = document.querySelectorAll('.share-btn')
 
 let isLinksActive = false
-let isMobile = false
 
 for(let i = 0; i < shareBtns.length; i++ ) {
     shareBtns[i].addEventListener('click', function() {
-
         this.classList.toggle('active')
 
         if(isLinksActive == false) {
             socials.style.display = 'flex'
+            if(window.innerWidth >= 900) {
+                userContainer.style.display = 'flex'
+            } else {
+                userContainer.style.display = 'none'
+            }
             isLinksActive = true
-        } else {
+        } else if (isLinksActive == true) {
             socials.style.display = 'none'
+            userContainer.style.display = 'flex'
             isLinksActive = false
         }
 
     })
 }
-
